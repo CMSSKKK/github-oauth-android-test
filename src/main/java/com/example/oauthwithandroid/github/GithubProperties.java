7afb134c16bc01b69ee632acef730212233b5db5
Minseok-Choi <cms05041@gmail.com>
2022-07-05 01:24:47 +0900

    fix

--- src/main/java/com/example/oauthwithandroid/github/GithubProperties.java
+++ src/main/java/com/example/oauthwithandroid/github/GithubProperties.java
@@ -1,16 +1,17 @@
 package com.example.oauthwithandroid.github;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
+import org.springframework.context.annotation.PropertySource;
 
 @Getter
-@RequiredArgsConstructor
+@AllArgsConstructor
 @NoArgsConstructor
 @ConfigurationProperties(prefix = "github")
+@Setter
+@PropertySource("classpath:auth.yml")
 public class GithubProperties {
 
     @JsonProperty("client_id")
