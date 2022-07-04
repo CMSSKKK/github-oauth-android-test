a4d8a26a2eff84be21544de5506a6edb7a669b74
Minseok-Choi <cms05041@gmail.com>
2022-07-05 01:09:08 +0900

    fix

--- src/main/java/com/example/oauthwithandroid/github/OauthController.java
+++ src/main/java/com/example/oauthwithandroid/github/OauthController.java
@@ -3,6 +3,7 @@ package com.example.oauthwithandroid.github;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -13,7 +14,7 @@ public class OauthController {
     private OauthService oauthService;
 
     @GetMapping("/github")
-    public GithubUserInfo userInfo(String code) {
+    public GithubUserInfo userInfo(@RequestParam String code) {
         log.info("code = {}", code);
         GithubToken githubToken = oauthService.requestAccessToken(code);
         log.info("githubtoken={}",githubToken.toHeader());
